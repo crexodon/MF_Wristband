f122c89900f7fc6043e3f72382cdd35c464eabb4
Crexodon <github@crexy.moe>
2025-02-08 00:55:40 +0100

    Working Code for Station

--- Wristband_Station/src/main.cpp
+++ Wristband_Station/src/main.cpp
@@ -3,32 +3,150 @@
 #include <SPI.h>
 #include <MFRC522.h>
 
-#define RST_PIN         21          // Configurable, see typical pin layout above
-#define SS_PIN          5         // Configurable, see typical pin layout above
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <Arduino_JSON.h>
 
+#include <secrets.h>
+
+#include <FastLED.h>
+#define NUM_LEDS 1
+#define DATA_PIN 14
+
+#define RST_PIN   21
+#define SS_PIN    5
+#define MOSI_PIN  23
+#define MISO_PIN  19
+#define CLK_PIN   18
+
+CRGB leds[NUM_LEDS];
 MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
 
+// Gets UID from tag and converts to string
+String convertUIDtoString(MFRC522::Uid *uid) {
+    // Print hex values
+    Serial.print("Hex values: ");
+    for(int i = 0; i < uid->size; i++) {
+        Serial.print(String(uid->uidByte[i], HEX));
+        Serial.print(" ");
+    }
+    Serial.println();
+    
+    // Convert bytes to string
+    char hexStr[20];  // Using fixed-size array instead of String
+    memset(hexStr, '0', sizeof(hexStr));
+    hexStr[sizeof(hexStr)-1] = '\0';
+    
+    int offset = 0;
+    for(int i = 0; i < uid->size; i++) {
+        sprintf(&hexStr[offset], "%02X", uid->uidByte[i]);
+        offset += 2;
+    }
+    
+    Serial.print("Final hex string: ");
+    Serial.println(hexStr);
+    
+    return hexStr;
+}
+
+// Sends string to REST API Endpoint. Returns http status code from response
+int sendTagNumber(String tag_id) {
+  WiFiClient client;
+  HTTPClient http;
+  
+  JSONVar jsonObject;
+  jsonObject["tag_id"] = tag_id;
+  
+  String jsonString = JSON.stringify(jsonObject);
+
+  String connection = rest_server + "/register";
+
+  Serial.println("Connection Name: " + connection);
+  
+  http.begin(client, connection);
+  http.addHeader("Content-Type", "application/json");
+  
+  int httpResponseCode = http.POST("{\"tag_id\":tag_id}");
+  
+  Serial.print("HTTP Response Code: ");
+  Serial.println(httpResponseCode);
+  Serial.println("Response:");
+  Serial.println(http.getString());
+  
+  http.end();
+  
+  return httpResponseCode;
+}
+
 void setup() {
-	Serial.begin(115200);		// Initialize serial communications with the PC
-	while (!Serial);		// Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
-	SPI.begin(18,19,23,SS_PIN);			// Init SPI bus
-	mfrc522.PCD_Init();		// Init MFRC522
-	delay(100);				// Optional delay. Some board do need more time after init to be ready, see Readme
-	mfrc522.PCD_DumpVersionToSerial();	// Show details of PCD - MFRC522 Card Reader details
-	Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
+	Serial.begin(115200);
+
+	SPI.begin(CLK_PIN, MISO_PIN, MOSI_PIN, SS_PIN);
+	mfrc522.PCD_Init();
+	
+  FastLED.addLeds<WS2812, DATA_PIN, RGB>(leds, NUM_LEDS);
+	
+  WiFi.begin(ssid, pass);
+  Serial.println("Connecting to WiFi");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(100);leds[0] = CRGB::Purple;
+    FastLED.show();
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println(WiFi.localIP());
+
+  leds[0] = CRGB::Yellow;
+  FastLED.show();
+
 }
 
 void loop() {
-	// Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
-	if ( ! mfrc522.PICC_IsNewCardPresent()) {
-		return;
-	}
+  while(WiFi.status() == WL_CONNECTED){
+    leds[0] = CRGB::Yellow;
+    FastLED.show();
 
-	// Select one of the cards
-	if ( ! mfrc522.PICC_ReadCardSerial()) {
-		return;
-	}
+	  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+    }
 
-	// Dump debug info about the card; PICC_HaltA() is automatically called
-	mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+    // Select one of the cards
+	  if ( ! mfrc522.PICC_ReadCardSerial()) {
+		  return;
+    }
+
+    // When Loop enters here, a card is present
+    leds[0] = CRGB::Blue;
+    FastLED.show();
+
+    //mfrc522.PICC_DumpToSerial(&(mfrc522.uid)); //Output Card Info
+
+	  String tag_id = convertUIDtoString(&mfrc522.uid);
+
+    Serial.printf("TagID: ", tag_id);
+      
+    int ret;
+    ret = sendTagNumber(tag_id);
+
+    if(ret == 200){
+        Serial.printf("Successfully sent tag number");
+        leds[0] = CRGB::Green;
+        FastLED.show();
+        delay(500);
+    } else {
+        Serial.println("Failed to send tag number with code: " + ret);
+        leds[0] = CRGB::Red;
+        FastLED.show();
+    }
+    
+    // Stop NFC readout
+    mfrc522.PICC_HaltA();
+    leds[0] = CRGB::Yellow;
+    FastLED.show();
+  }
+
+  Serial.print("WiFi not connected...");
+  leds[0] = CRGB::Purple;
+  FastLED.show();
+  delay(200);
 }
